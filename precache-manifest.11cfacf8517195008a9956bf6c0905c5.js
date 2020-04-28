b6b8264313eff9b4d00a5cf1198f429bd07e908b
themiee <navyblack16@yahoo.com>
2020-04-28 07:52:18 +0100

    Updates

---
+++ precache-manifest.11cfacf8517195008a9956bf6c0905c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "938c68b77389d7bc82c5840018dfe09c",
+    "url": "/React-Color_Boxes/index.html"
+  },
+  {
+    "revision": "d5dcd928e30932b6f703",
+    "url": "/React-Color_Boxes/static/css/main.8f54b82e.chunk.css"
+  },
+  {
+    "revision": "ad893f85c9534d6a1ffa",
+    "url": "/React-Color_Boxes/static/js/2.9eea1258.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Color_Boxes/static/js/2.9eea1258.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5dcd928e30932b6f703",
+    "url": "/React-Color_Boxes/static/js/main.e75e598a.chunk.js"
+  },
+  {
+    "revision": "4d9c3bfe2f1b98db3f82",
+    "url": "/React-Color_Boxes/static/js/runtime-main.e3f5a3a9.js"
+  }
+]);
